3dcbf9ca7445fe2a46a229e6a030286b9e1528f8
Luismi <luismi.gamo@gmail.com>
2017-04-13 11:52:49 +0200

    Added test for kafka data ingestion

---
+++ tests/test_KafkaConsumer.py
@@ -0,0 +1,10 @@
+from kafka import KafkaConsumer
+
+# To consume latest messages and auto-commit offsets
+consumer = KafkaConsumer('LensJobs',
+                         group_id='LensJobsGID',
+                         bootstrap_servers=['192.168.1.111:9092'])
+for message in consumer:
+       print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
